dd5fb0e2240e09d7779803f77d07d23772a4418e
Orhan Uckulac <=>
2022-11-06 18:37:59 +0300

    first commit - setup add place activity and design

---
+++ app/src/main/java/orhan/uckulac/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,20 @@
+package orhan.uckulac.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import orhan.uckulac.happyplaces.databinding.ActivityAddHappyPlaceBinding
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    private var binding: ActivityAddHappyPlaceBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
